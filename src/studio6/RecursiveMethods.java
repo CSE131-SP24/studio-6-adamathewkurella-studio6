c45aec135f6eb1d66255a4f499393028e8918a0e
isabe <isabe@IMComputer.dhcp.wustl.edu>
2024-03-06 15:48:11 -0600

    rah did nothing

--- src/studio6/RecursiveMethods.java
+++ src/studio6/RecursiveMethods.java
@@ -13,8 +13,18 @@ public class RecursiveMethods {
 	 */
 	public static double geometricSum(int n) {
 		
-			// FIXME compute the geometric sum for the first n terms recursively
+		if (n == 0)
+		{
 			return 0;
+		}
+		else if (n == 1)
+		{
+			return 0.5;
+		}
+		else 
+		{
+			return Math.pow(0.5,  n) + geometricSum(n-1); 
+		}
 		
 	}
 
@@ -29,7 +39,15 @@ public class RecursiveMethods {
 	public static int gcd(int p, int q) {
 		
 			// FIXME compute the gcd of p and q using recursion
-			return 0;
+			
+		if (q == 0)
+		{
+			return p;
+		}
+		else
+		{
+			return gcd(q, p%q);
+		}
 		
 	}
 
